db17ca95f632758eebff36cdcec7192d9d27fcd4
Pedro533237 <105571750+Pedro533237@users.noreply.github.com>
2026-04-18 12:15:07 -0300

    Update HandledScreenMixin.java

--- src/client/java/com/barium/client/mixin/HandledScreenMixin.java
+++ src/client/java/com/barium/client/mixin/HandledScreenMixin.java
@@ -3,7 +3,7 @@ package com.barium.client.mixin;
 import com.barium.client.util.TooltipManager;
 import com.barium.config.BariumConfig;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
