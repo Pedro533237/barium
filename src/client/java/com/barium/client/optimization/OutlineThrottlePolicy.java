206b5231720cc6b7e0ca57c261dfc8ca40607c96
Pedro533237 <105571750+Pedro533237@users.noreply.github.com>
2026-04-19 14:33:06 -0300

    Soften outline throttle policy to reduce stutter

--- src/client/java/com/barium/client/optimization/OutlineThrottlePolicy.java
+++ src/client/java/com/barium/client/optimization/OutlineThrottlePolicy.java
@@ -3,26 +3,24 @@ package com.barium.client.optimization;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class OutlineThrottlePolicy {
+	private static final AtomicInteger HOT_FRAME_STREAK = new AtomicInteger(0);
 	private static final AtomicInteger OUTLINE_FRAME_COUNTER = new AtomicInteger(0);
 
 	private OutlineThrottlePolicy() {
 	}
 
 	public static boolean shouldRenderOutline(long averageFrameNanos) {
-		int step = calculateStep(averageFrameNanos);
+		if (averageFrameNanos < 20_000_000L) {
+			HOT_FRAME_STREAK.set(0);
+			return true;
+		}
+
+		int streak = HOT_FRAME_STREAK.updateAndGet(previous -> Math.min(previous + 1, 600));
+		if (streak < 120) {
+			return true;
+		}
+
 		int frame = OUTLINE_FRAME_COUNTER.incrementAndGet();
-		return frame % step == 0;
-	}
-
-	private static int calculateStep(long averageFrameNanos) {
-		if (averageFrameNanos >= 28_000_000L) {
-			return 4;
-		}
-
-		if (averageFrameNanos >= 20_000_000L) {
-			return 2;
-		}
-
-		return 1;
+		return frame % 2 == 0;
 	}
 }
