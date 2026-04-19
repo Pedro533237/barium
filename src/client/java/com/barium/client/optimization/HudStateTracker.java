06212dfeb86f60d8d7d74df8c4cea1582b9dcf9b
Pedro533237 <105571750+Pedro533237@users.noreply.github.com>
2026-04-19 14:49:22 -0300

    Implement state-aware GUI throttling and remove tick skipping

---
+++ src/client/java/com/barium/client/optimization/HudStateTracker.java
@@ -0,0 +1,57 @@
+package com.barium.client.optimization;
+
+import net.minecraft.client.player.LocalPlayer;
+
+public final class HudStateTracker {
+	private static int lastFood;
+	private static int lastArmor;
+	private static int lastAir;
+	private static int lastXpLevel;
+	private static float lastHealth;
+	private static float lastXpProgress;
+	private static int lastHotbarSlot;
+	private static boolean initialized;
+
+	private HudStateTracker() {
+	}
+
+	public static boolean hasHudStateChanged(LocalPlayer player) {
+		int food = player.getFoodData().getFoodLevel();
+		int armor = player.getArmorValue();
+		int air = player.getAirSupply();
+		int xpLevel = player.experienceLevel;
+		float health = player.getHealth();
+		float xpProgress = player.experienceProgress;
+		int hotbarSlot = player.getInventory().selected;
+
+		if (!initialized) {
+			store(food, armor, air, xpLevel, health, xpProgress, hotbarSlot);
+			initialized = true;
+			return true;
+		}
+
+		boolean changed = food != lastFood
+			|| armor != lastArmor
+			|| air != lastAir
+			|| xpLevel != lastXpLevel
+			|| Float.compare(health, lastHealth) != 0
+			|| Float.compare(xpProgress, lastXpProgress) != 0
+			|| hotbarSlot != lastHotbarSlot;
+
+		if (changed) {
+			store(food, armor, air, xpLevel, health, xpProgress, hotbarSlot);
+		}
+
+		return changed;
+	}
+
+	private static void store(int food, int armor, int air, int xpLevel, float health, float xpProgress, int hotbarSlot) {
+		lastFood = food;
+		lastArmor = armor;
+		lastAir = air;
+		lastXpLevel = xpLevel;
+		lastHealth = health;
+		lastXpProgress = xpProgress;
+		lastHotbarSlot = hotbarSlot;
+	}
+}
