100397543ad2a3453b19c6c10949a6d892f73cdf
Pedro533237 <105571750+Pedro533237@users.noreply.github.com>
2026-06-07 14:55:25 -0300

    Refactor endFrame method for atomic updates

--- src/client/java/com/barium/client/optimization/FrameTimeTracker.java
+++ src/client/java/com/barium/client/optimization/FrameTimeTracker.java
@@ -3,21 +3,26 @@ package com.barium.client.optimization;
 import java.util.concurrent.atomic.AtomicLong;
 
 public final class FrameTimeTracker {
-	private static final AtomicLong AVERAGE_FRAME_NANOS = new AtomicLong(16_000_000L);
+    private static final AtomicLong AVERAGE_FRAME_NANOS = new AtomicLong(16_000_000L);
 
-	private FrameTimeTracker() {
-	}
+    private FrameTimeTracker() {
+    }
 
-	public static long beginFrame() {
-		return System.nanoTime();
-	}
+    public static long beginFrame() {
+        return System.nanoTime();
+    }
 
-	public static void endFrame(long startedAtNanos) {
-		long frameTime = Math.max(0L, System.nanoTime() - startedAtNanos);
-		AVERAGE_FRAME_NANOS.updateAndGet(previous -> ((previous * 7L) + frameTime) / 8L);
-	}
+    public static void endFrame(long startedAtNanos) {
+        long frameTime = Math.max(0L, System.nanoTime() - startedAtNanos);
+        long previous;
+        long next;
+        do {
+            previous = AVERAGE_FRAME_NANOS.get();
+            next = ((previous * 7L) + frameTime) >> 3;
+        } while (!AVERAGE_FRAME_NANOS.compareAndSet(previous, next));
+    }
 
-	public static long getAverageFrameNanos() {
-		return AVERAGE_FRAME_NANOS.get();
-	}
+    public static long getAverageFrameNanos() {
+        return AVERAGE_FRAME_NANOS.get();
+    }
 }
